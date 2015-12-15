b79800c0f762267ecd912c9e888a6afc59b387ca
sailor103 <sailor9066@gmail.com>
2015-12-15 10:16:13 +0800

    画点的代码实现

--- malagis/malaBase/_malaBase.cpp
+++ malagis/malaBase/_malaBase.cpp
@@ -42,20 +42,67 @@ malaCDC::~malaCDC()
 */
 void malaCDC::pointDrawCircle(malaPoint Point, malaPointPro PntPro)
 {
-	MessageBox(NULL, L"圆形", _T("提示"), MB_OK);
+	//MessageBox(NULL, L"绘制圆形", _T("提示"), MB_OK);
+	CClientDC dc(mView);
+
+	CPen pen(0, 1, PntPro.pointColor);
+	CPen* OldPen = dc.SelectObject(&pen);
+	CBrush MyBrush(PntPro.pointColor);
+	CBrush* OldBrush = dc.SelectObject(&MyBrush);
+	CPoint Point1, Point2;
+	Point1.x = Point.x - PntPro.pointRadio;
+	Point1.y = Point.y - PntPro.pointRadio;
+	Point2.x = Point.x + PntPro.pointRadio;
+	Point2.y = Point.y + PntPro.pointRadio;
+	dc.Ellipse(CRect(Point1, Point2));
+	dc.SelectObject(OldPen);
+	dc.SelectObject(OldBrush);
 }
 /*
 * 绘制三角形点
 */
 void malaCDC::pointDrawTriangle(malaPoint Point, malaPointPro PntPro)
 {
-	MessageBox(NULL, L"三角形", _T("提示"), MB_OK);
+	//MessageBox(NULL, L"绘制三角形", _T("提示"), MB_OK);
+	CClientDC dc(mView);
+	CPen pen(0, 1, PntPro.pointColor);
+	CPen* OldPen = dc.SelectObject(&pen);
+	CBrush MyBrush(PntPro.pointColor);
+	CBrush* OldBrush = dc.SelectObject(&MyBrush);
+	double radio = PntPro.pointRadio;
+	double r1 = sqrtf(3) / 2 * radio;
+	CPoint* MyArray = new CPoint[3];
+	MyArray[0].x = Point.x - r1;
+	MyArray[0].y = Point.y + (double)radio/2;
+	MyArray[1].x = Point.x + r1;
+	MyArray[1].y = Point.y + (double)radio / 2;
+	MyArray[2].x = Point.x;
+	MyArray[2].y = Point.y-radio;
+
+	dc.Polygon(MyArray, 3);
+	dc.SelectObject(OldPen);
+	dc.SelectObject(OldBrush);
 }
 /*
 * 绘制矩（方）形点
 */
 void malaCDC::pointDrawRect(malaPoint Point, malaPointPro PntPro)
 {
-	MessageBox(NULL, L"方形", _T("提示"), MB_OK);
+	//MessageBox(NULL, L"绘制方形", _T("提示"), MB_OK);
+	CClientDC dc(mView);
+
+	CPen pen(0, 1, PntPro.pointColor);
+	CPen* OldPen = dc.SelectObject(&pen);
+	CBrush MyBrush(PntPro.pointColor);
+	CBrush* OldBrush = dc.SelectObject(&MyBrush);
+	CPoint Point1, Point2;
+	Point1.x = Point.x - PntPro.pointRadio;
+	Point1.y = Point.y - PntPro.pointRadio;
+	Point2.x = Point.x + PntPro.pointRadio;
+	Point2.y = Point.y + PntPro.pointRadio;
+
+	dc.Rectangle(CRect(Point1, Point2));
+	dc.SelectObject(OldPen);
+	dc.SelectObject(OldBrush);
 
 }
